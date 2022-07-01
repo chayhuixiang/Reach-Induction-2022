fe97563818b193928f3a7cb30cde05a2b8550eba
Chay Hui Xiang <75465219+chayhuixiang@users.noreply.github.com>
2022-07-02 01:50:27 +0800

    Edited readme.md

---
+++ Arduino_Basics/Serial/Serial.ino
@@ -0,0 +1,14 @@
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  const char output[20] = "Hello World";
+  // print out the state of the button:
+  Serial.println(output);
+  delay(1);        // delay in between reads for stability
+}
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Session Plan:
 
 1. Intro & Installation of Arduino [Download](https://www.arduino.cc/en/software) [10mins]
-2. Arduino Basics [20mins]
-3. Arduino Demos [5mins]
-4. Frontend [10mins]
-5. Backend [10mins]
+2. Arduino Basics [Code](./Arduino_Basics/) [20mins]
+3. Arduino Demos [Code](./Arduino_Demo/) [5mins]
+5. Backend [Code](./Backend/) [10mins]
+4. Frontend [Code](./Frontend/) [10mins]
